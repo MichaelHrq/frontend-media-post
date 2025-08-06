7047e8e655195cdb7d9c46bdfd9a12202e89feea
Michael <michael@clickmulti.com.br>
2025-08-06 17:46:47 -0400

    first commit

---
+++ src/constants/modelo.ts
@@ -0,0 +1,44 @@
+export const modelo = {
+  feed_1_1: {
+    title: 'Feed Quadrado',
+    aspect: 1/1,
+    width: 1080,
+    height: 1080,
+    overlayPath: "/feed.png",
+  },
+  feed_4_5: {
+    title: 'Feed Retrato',
+    aspect: 4/5,
+    width: 1440,
+    height: 1800,
+    overlayPath: "/feed.png",
+  },
+  story: {
+    title: 'Story',
+    aspect: 9/16,
+    width: 1080,
+    height: 1920,
+    overlayPath: "/story.png",
+  },
+  reels: {
+    title: 'Reels',
+    aspect: 9/16,
+    width: 1080,
+    height: 1920,
+    overlayPath: "/story.png",
+  },
+  marketplace: {
+    title: 'Marketplace',
+    aspect: 1/1,
+    width: 1200,
+    height: 1200,
+    overlayPath: "/feed.png",
+  },
+  colDireitaFb: {
+    title: 'Coluna Facebook',
+    aspect: 1/1,
+    width: 1200,
+    height: 1200,
+    overlayPath: "/feed.png",
+  },
+};
