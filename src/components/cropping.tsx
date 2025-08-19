d201c726670f3eae3e1d8547832267263190b21f
Michael <michael@clickmulti.com.br>
2025-08-19 11:06:12 -0400

    fix: images crossOrigin anonymous

--- src/components/cropping.tsx
+++ src/components/cropping.tsx
@@ -22,6 +22,7 @@ interface CroppingProps {
 const createImage = (url: string): Promise<HTMLImageElement> =>
   new Promise((resolve, reject) => {
     const image = new Image();
+    image.crossOrigin = "anonymous";
     image.addEventListener("load", () => resolve(image));
     image.addEventListener("error", (error) => reject(error));
     image.setAttribute("crossOrigin", "anonymous");
