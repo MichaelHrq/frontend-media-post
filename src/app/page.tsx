f3905e82a11e0e146b1ff82a1d4bcbdb64f002e1
Michael Henrique <michael@clickmulti.com.br>
2025-08-25 13:02:49 -0300

    correcao git config email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import Cropping from "@/components/cropping";
 import ModelSelect from "@/components/modelo";
+import Preview from "@/components/preview";
 import Selection from "@/components/selection";
 import Template from "@/components/template";
+import { cenariumModel } from "@/constants/modelos/cenarium";
+import { emtempoModel } from "@/constants/modelos/emtempo";
+import { cenariumTemplate } from "@/constants/templates/cenarium";
+import { emtempoTemplate } from "@/constants/templates/emtempo";
 import { useCallback, useEffect, useState } from "react";
 import { fetchData } from "./action";
-import Cropping from "@/components/cropping";
-import Preview from "@/components/preview";
-import { emtempoModel } from "@/constants/modelos/emtempo";
-import { cenariumModel } from "@/constants/modelos/cenarium";
-import { emtempoTemplate } from "@/constants/templates/emtempo";
 import {
   configType,
   newsType,
@@ -17,7 +18,6 @@ import {
   stepType,
   templateType,
 } from "./type";
-import { cenariumTemplate } from "@/constants/templates/cenarium";
 
 export default function Home() {
   const [step, setStep] = useState<stepType>("model");
